294c1817613b3590c5b7e718c3d2ab832c07d46c
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-01-06 23:16:06 +0100

    added volume/brightness/play and pause controls

---
+++ android/src/main/kotlin/gg/HmZyy/fancy_video_player/Functions.kt
@@ -0,0 +1,172 @@
+package gg.HmZyy
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.os.PowerManager
+import android.provider.Settings
+import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import androidx.core.math.MathUtils
+import com.google.android.exoplayer2.ui.DefaultTimeBar
+import java.lang.reflect.Field
+import java.util.*
+import kotlin.math.log2
+import kotlin.math.pow
+
+@SuppressLint("ClickableViewAccessibility")
+class SpinnerNoSwipe : androidx.appcompat.widget.AppCompatSpinner {
+    private var mGestureDetector: GestureDetector? = null
+
+    constructor(context: Context) : super(context) {
+        setup()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        setup()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        setup()
+    }
+
+    private fun setup() {
+        mGestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                return performClick()
+            }
+        })
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        mGestureDetector!!.onTouchEvent(event)
+        return true
+    }
+}
+
+@SuppressLint("ViewConstructor")
+class ExtendedTimeBar(
+    context: Context,
+    attrs: AttributeSet?
+) : DefaultTimeBar(context, attrs) {
+    private var enabled = false
+    private var forceDisabled = false
+    override fun setEnabled(enabled: Boolean) {
+        this.enabled = enabled
+        super.setEnabled(!forceDisabled && this.enabled)
+    }
+
+    fun setForceDisabled(forceDisabled: Boolean) {
+        this.forceDisabled = forceDisabled
+        isEnabled = enabled
+    }
+}
+
+abstract class GesturesListener : GestureDetector.SimpleOnGestureListener() {
+    private var timer: Timer? = null //at class level;
+    private val delay: Long = 200
+
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+        processSingleClickEvent(e)
+        return super.onSingleTapUp(e)
+    }
+
+    override fun onLongPress(e: MotionEvent) {
+        processLongClickEvent(e)
+        super.onLongPress(e)
+    }
+
+    override fun onDoubleTap(e: MotionEvent): Boolean {
+        processDoubleClickEvent(e)
+        return super.onDoubleTap(e)
+    }
+
+    override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+        onScrollYClick(distanceY)
+        onScrollXClick(distanceX)
+        return super.onScroll(e1, e2, distanceX, distanceY)
+    }
+
+    private fun processSingleClickEvent(e: MotionEvent) {
+        val handler = Handler(Looper.getMainLooper())
+        val mRunnable = Runnable {
+            onSingleClick(e)
+        }
+        timer = Timer().apply {
+            schedule(object : TimerTask() {
+                override fun run() {
+                    handler.post(mRunnable)
+                }
+            }, delay)
+        }
+    }
+
+    private fun processDoubleClickEvent(e: MotionEvent) {
+        timer?.apply {
+            cancel()
+            purge()
+        }
+        onDoubleClick(e)
+    }
+
+    private fun processLongClickEvent(e: MotionEvent) {
+        timer?.apply {
+            cancel()
+            purge()
+        }
+        onLongClick(e)
+    }
+
+    open fun onSingleClick(event: MotionEvent) {}
+    open fun onDoubleClick(event: MotionEvent) {}
+    open fun onScrollYClick(y: Float) {}
+    open fun onScrollXClick(y: Float) {}
+    open fun onLongClick(event: MotionEvent) {}
+}
+
+@Suppress("DEPRECATION")
+fun Activity.hideSystemBars() {
+    window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            )
+}
+fun getCurrentBrightnessValue(context: Context): Float {
+    fun getMax(): Int {
+        val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+
+        val fields: Array<Field> = powerManager.javaClass.declaredFields
+        for (field in fields) {
+            if (field.name.equals("BRIGHTNESS_ON")) {
+                field.isAccessible = true
+                return try {
+                    field.get(powerManager)?.toString()?.toInt() ?: 255
+                } catch (e: IllegalAccessException) {
+                    255
+                }
+            }
+        }
+        return 255
+    }
+
+    fun getCur(): Float {
+        return Settings.System.getInt(context.contentResolver, Settings.System.SCREEN_BRIGHTNESS, 127).toFloat()
+    }
+
+    return brightnessConverter(getCur() / getMax(), true)
+}
+
+fun brightnessConverter(it: Float, fromLog: Boolean) =
+    MathUtils.clamp(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+            if (fromLog) log2((it * 256f)) * 12.5f / 100f else 2f.pow(it * 100f / 12.5f) / 256f
+        else it, 0.001f, 1f
+    )
+
--- android/src/main/kotlin/gg/HmZyy/fancy_video_player/PlayerActivity.kt
+++ android/src/main/kotlin/gg/HmZyy/fancy_video_player/PlayerActivity.kt
@@ -1,17 +1,42 @@
 package gg.HmZyy.fancy_video_player
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.annotation.SuppressLint
+import android.content.res.Configuration
+import android.graphics.drawable.Animatable
+import android.media.AudioManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.core.math.MathUtils
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.updateLayoutParams
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.exoplayer2.util.Util
+import com.google.android.material.slider.Slider
+import gg.HmZyy.GesturesListener
+import gg.HmZyy.brightnessConverter
 import gg.HmZyy.fancy_video_player.databinding.ActivityPlayerBinding
+import gg.HmZyy.getCurrentBrightnessValue
+import gg.HmZyy.hideSystemBars
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.math.roundToInt
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -21,6 +46,25 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var player: ExoPlayer
     private lateinit var binding: ActivityPlayerBinding
     private lateinit var videoUri: String
+
+    // States
+    private var isInitialized = false
+    private var isPlayerPlaying = true
+
+    // Options
+    private lateinit var exoPlay: ImageButton
+    private lateinit var playerView: StyledPlayerView
+    private lateinit var exoBrightness: Slider
+    private lateinit var exoBrightnessCont: View
+    private lateinit var exoVolume: Slider
+    private lateinit var exoVolumeCont: View
+
+    // Handlers
+    private val handler = Handler(Looper.getMainLooper())
+
+    //
+    private var notchHeight: Int = 0
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +72,8 @@ class PlayerActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
         videoUri = intent.getStringExtra("url") ?: return
+        setupPlayer()
+        hideSystemBars()
     }
 
     public override fun onStart() {
@@ -57,6 +103,173 @@ class PlayerActivity : AppCompatActivity() {
         player.prepare()
         // Start the playback.
         player.play()
+        isInitialized = true
+    }
+
+    private fun setupPlayer(){
+        playerView = findViewById(R.id.player_view)
+        exoBrightness = playerView.findViewById(R.id.exo_brightness)
+        exoBrightnessCont = playerView.findViewById(R.id.exo_brightness_cont)
+        exoPlay = playerView.findViewById(R.id.exo_play)
+        exoVolume = playerView.findViewById(R.id.exo_volume)
+        exoVolumeCont = playerView.findViewById(R.id.exo_volume_cont)
+
+
+        //Play Pause
+        exoPlay.setOnClickListener {
+            if (isInitialized) {
+                isPlayerPlaying = player.isPlaying
+                (exoPlay.drawable as Animatable?)?.start()
+                if (isPlayerPlaying) {
+                    Glide.with(this).load(R.drawable.anim_pause_to_play).into(exoPlay)
+                    player.pause()
+                } else {
+                    Glide.with(this).load(R.drawable.anim_play_to_pause).into(exoPlay)
+                    player.play()
+                }
+            }
+        }
+        setupGestures()
+    }
+
+    private fun setupGestures () {
+        var locked = false
+
+        val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+        var animationSpeed: Float = 1f
+        val gestureSpeed = (300 * animationSpeed).toLong()
+
+
+        //Player UI Visibility Handler
+        val brightnessRunnable = Runnable {
+            if (exoBrightnessCont.alpha == 1f)
+                lifecycleScope.launch {
+                    ObjectAnimator.ofFloat(exoBrightnessCont, "alpha", 1f, 0f).setDuration(gestureSpeed).start()
+                    delay(gestureSpeed)
+                    exoBrightnessCont.visibility = View.GONE
+                }
+        }
+        val volumeRunnable = Runnable {
+            if (exoVolumeCont.alpha == 1f)
+                lifecycleScope.launch {
+                    ObjectAnimator.ofFloat(exoVolumeCont, "alpha", 1f, 0f).setDuration(gestureSpeed).start()
+                    delay(gestureSpeed)
+                    exoVolumeCont.visibility = View.GONE
+                }
+        }
+        playerView.setControllerVisibilityListener(StyledPlayerView.ControllerVisibilityListener { visibility ->
+            if (visibility == View.GONE) {
+                hideSystemBars()
+                brightnessRunnable.run()
+                volumeRunnable.run()
+            }
+        })
+
+        fun doubleTap(forward: Boolean, event: MotionEvent) {
+//            if (!locked && isInitialized && settings.doubleTap) {
+//                seek(forward, event)
+//            }
+        }
+
+        //Brightness
+        var brightnessTimer = Timer()
+        exoBrightnessCont.visibility = View.GONE
+
+        fun brightnessHide() {
+            brightnessTimer.cancel()
+            brightnessTimer.purge()
+            val timerTask: TimerTask = object : TimerTask() {
+                override fun run() {
+                    handler.post(brightnessRunnable)
+                }
+            }
+            brightnessTimer = Timer()
+            brightnessTimer.schedule(timerTask, 3000)
+        }
+        exoBrightness.value = (getCurrentBrightnessValue(this) * 10f)
+
+        exoBrightness.addOnChangeListener { _, value, _ ->
+            val lp = window.attributes
+            lp.screenBrightness = brightnessConverter(value / 10, false)
+            window.attributes = lp
+            brightnessHide()
+        }
+
+        //Volume
+        var volumeTimer = Timer()
+        exoVolumeCont.visibility = View.GONE
+
+        val volumeMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        exoVolume.value = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC).toFloat() / volumeMax * 10
+        fun volumeHide() {
+            volumeTimer.cancel()
+            volumeTimer.purge()
+            val timerTask: TimerTask = object : TimerTask() {
+                override fun run() {
+                    handler.post(volumeRunnable)
+                }
+            }
+            volumeTimer = Timer()
+            volumeTimer.schedule(timerTask, 3000)
+        }
+        exoVolume.addOnChangeListener { _, value, _ ->
+            val volume = (value / 10 * volumeMax).roundToInt()
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0)
+            volumeHide()
+        }
+
+        //FastRewind (Left Panel)
+        val fastRewindDetector = GestureDetector(this, object : GesturesListener() {
+            override fun onDoubleClick(event: MotionEvent) {
+                doubleTap(false, event)
+            }
+
+            override fun onScrollYClick(y: Float) {
+                if (!locked) {
+                    exoBrightness.value =
+                        MathUtils.clamp(exoBrightness.value + y / 100, 0f, 10f)
+                    if (exoBrightnessCont.visibility != View.VISIBLE) {
+                        exoBrightnessCont.visibility = View.VISIBLE
+                    }
+                    exoBrightnessCont.alpha = 1f
+                }
+            }
+
+//            override fun onSingleClick(event: MotionEvent) = handleController()
+        })
+        val rewindArea = playerView.findViewById<View>(R.id.exo_rewind_area)
+        rewindArea.isClickable = true
+        rewindArea.setOnTouchListener { v, event ->
+            fastRewindDetector.onTouchEvent(event)
+            v.performClick()
+            true
+        }
+
+        //FastForward (Right Panel)
+        val fastForwardDetector = GestureDetector(this, object : GesturesListener() {
+            override fun onDoubleClick(event: MotionEvent) {
+                doubleTap(true, event)
+            }
+
+            override fun onScrollYClick(y: Float) {
+                if (!locked) {
+                    exoVolume.value = MathUtils.clamp(exoVolume.value + y / 100, 0f, 10f)
+                    if (exoVolumeCont.visibility != View.VISIBLE) {
+                        exoVolumeCont.visibility = View.VISIBLE
+                    }
+                    exoVolumeCont.alpha = 1f
+                }
+            }
+
+//            override fun onSingleClick(event: MotionEvent) = handleController()
+        })
+        val forwardArea = playerView.findViewById<View>(R.id.exo_forward_area)
+        forwardArea.isClickable = true
+        forwardArea.setOnTouchListener { v, event ->
+            fastForwardDetector.onTouchEvent(event)
+            v.performClick()
+            true
+        }
     }
 
     @SuppressLint("InlinedApi")
@@ -67,7 +280,6 @@ class PlayerActivity : AppCompatActivity() {
             controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
-
 
     public override fun onPause() {
         super.onPause()
